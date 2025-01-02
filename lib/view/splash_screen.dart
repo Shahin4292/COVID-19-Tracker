fd5fe3134ae871535d4531d9d1d634e8395cf197
Shahin Sarker <141581883+Shahin4292@users.noreply.github.com>
2025-01-02 18:46:32 +0600

    almost done animated splash screen

--- lib/view/splash_screen.dart
+++ lib/view/splash_screen.dart
@@ -16,10 +16,17 @@ class _SplashScreenState extends State<SplashScreen>
   )..repeat();
 
   @override
+  void initState() {
+    super.initState();
+  }
+  @override
   Widget build(BuildContext context) {
+    Size size = MediaQuery.sizeOf(context);
     return Scaffold(
       body: SafeArea(
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
           children: [
             AnimatedBuilder(
                 animation: _controller,
@@ -28,7 +35,16 @@ class _SplashScreenState extends State<SplashScreen>
                     angle: _controller.value * 2.0 * math.pi,
                     child: child,
                   );
-                })
+                },
+                child: Center(
+                  child: Container(
+                    height: size.height * 0.35,
+                    width: size.width * 0.35,
+                    child: const Image(
+                      image: AssetImage('images/virus.png'),
+                    ),
+                  ),
+                ))
           ],
         ),
       ),
