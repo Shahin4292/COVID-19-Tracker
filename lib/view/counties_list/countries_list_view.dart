1d0ce1bbd82274e7dc172972e8f146add8b47579
Shahin Sarker <141581883+Shahin4292@users.noreply.github.com>
2025-01-03 15:42:00 +0600

    add text field

---
+++ lib/view/counties_list/countries_list_view.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+
+import '../../viewModel/service/covid_19_service.dart';
+
+class CountriesListView extends StatefulWidget {
+  const CountriesListView({super.key});
+
+  @override
+  State<CountriesListView> createState() => _CountriesListViewState();
+}
+
+class _CountriesListViewState extends State<CountriesListView> {
+  TextEditingController searchController = TextEditingController();
+  Covid19Service covid19service = Covid19Service();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey.shade800,
+      appBar: AppBar(
+        elevation: 0,
+        backgroundColor: Colors.transparent,
+      ),
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            children: [
+              TextFormField(
+                style: const TextStyle(color: Colors.white),
+                controller: searchController,
+                decoration: InputDecoration(
+                    contentPadding: const EdgeInsets.symmetric(horizontal: 20),
+                    border: OutlineInputBorder(
+                      borderSide:
+                          const BorderSide(width: 3, color: Colors.white),
+                      borderRadius: BorderRadius.circular(12),
+                    ),
+                    hintStyle: const TextStyle(color: Colors.white),
+                    hintText: 'Search with countries name'),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
