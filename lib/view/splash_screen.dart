40f1bd33e50de78ebf020b4a2b4a7b0731157866
Shahin Sarker <141581883+Shahin4292@users.noreply.github.com>
2025-01-03 07:46:36 +0600

    all are done

--- lib/view/splash_screen.dart
+++ lib/view/splash_screen.dart
@@ -1,3 +1,6 @@
+import 'dart:async';
+
+import 'package:covid_19/view/home_screen.dart';
 import 'package:flutter/material.dart';
 import 'dart:math' as math;
 
@@ -18,34 +21,62 @@ class _SplashScreenState extends State<SplashScreen>
   @override
   void initState() {
     super.initState();
+    Timer(
+      const Duration(seconds: 3),
+      () => Navigator.push(
+        context,
+        MaterialPageRoute(
+          builder: (context) => const HomeScreen(),
+        ),
+      ),
+    );
   }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.sizeOf(context);
     return Scaffold(
+      backgroundColor: Colors.grey.shade800,
       body: SafeArea(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            AnimatedBuilder(
-                animation: _controller,
-                builder: (BuildContext context, Widget? child) {
-                  return Transform.rotate(
-                    angle: _controller.value * 2.0 * math.pi,
-                    child: child,
-                  );
-                },
-                child: Center(
-                  child: Container(
+        child: Center(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              AnimatedBuilder(
+                  animation: _controller,
+                  builder: (BuildContext context, Widget? child) {
+                    return Transform.rotate(
+                      angle: _controller.value * 2.0 * math.pi,
+                      child: child,
+                    );
+                  },
+                  child: SizedBox(
                     height: size.height * 0.35,
                     width: size.width * 0.35,
                     child: const Image(
                       image: AssetImage('images/virus.png'),
                     ),
-                  ),
-                ))
-          ],
+                  )),
+              SizedBox(
+                height: size.height * 0.05,
+              ),
+              const Text(
+                textAlign: TextAlign.center,
+                "Covid-19\nTracker",
+                style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 25,
+                    color: Colors.white),
+              )
+            ],
+          ),
         ),
       ),
     );
