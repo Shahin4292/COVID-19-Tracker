cbb8ee29f8883419552c8a202d6072f7c5cf836e
Shahin Sarker <141581883+Shahin4292@users.noreply.github.com>
2025-01-03 16:52:21 +0600

    add complete

---
+++ lib/viewModel/service/covid_19_service.dart
@@ -0,0 +1,38 @@
+import 'dart:convert';
+
+import 'package:covid_19/res/app_url/app_url.dart';
+import 'package:http/http.dart' as http;
+
+import '../../model/covid_countries_model.dart';
+import '../../model/covid_model.dart';
+
+class Covid19Service {
+  // List<CovidCountriesModel> countriesList = [];
+
+  Future<CovidModel?> getCovidApi() async {
+    final response = await http.get(Uri.parse(AppUrl.covid19Api));
+    var data = jsonDecode(response.body.toString());
+    if (response.statusCode == 200) {
+      return CovidModel.fromJson(data);
+    } else {
+      throw Exception('error');
+    }
+  }
+
+  Future<dynamic> getCovidCountriesApi() async {
+    var data;
+    final response = await http.get(Uri.parse(AppUrl.countriesApi));
+    print(response.statusCode.toString());
+    print(data);
+    if (response.statusCode == 200) {
+      var data = jsonDecode(response.body.toString());
+      return data;
+      // for (int i = 0; i < data.length; i++) {
+      //   data.add(CovidCountriesModel.fromJson(dataJson[i]));
+      // }
+    } else {
+      throw Exception('error');
+    }
+    return null;
+  }
+}
