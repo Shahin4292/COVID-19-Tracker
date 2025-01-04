5b0120b9625173cac54f1762349d5c7e0f2ae9cc
Shahin Sarker <141581883+Shahin4292@users.noreply.github.com>
2025-01-04 10:46:31 +0600

    all add doe

--- lib/view/details_view/details_view.dart
+++ lib/view/details_view/details_view.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import '../home/widget/reusable_row.dart';
+
 class DetailsView extends StatefulWidget {
   String image;
   String name;
@@ -11,17 +13,16 @@ class DetailsView extends StatefulWidget {
       todayRecovered,
       test;
 
-  DetailsView(
-      {super.key,
-      required this.image,
-      required this.active,
-      required this.critical,
-      required this.name,
-      required this.totalCases,
-      required this.totalDeaths,
-      required this.todayRecovered,
-      required this.totalRecovered,
-      required this.test});
+  DetailsView({super.key,
+    required this.image,
+    required this.active,
+    required this.critical,
+    required this.name,
+    required this.totalCases,
+    required this.totalDeaths,
+    required this.todayRecovered,
+    required this.totalRecovered,
+    required this.test});
 
   @override
   State<DetailsView> createState() => _DetailsViewState();
@@ -31,10 +32,45 @@ class _DetailsViewState extends State<DetailsView> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.name),
-        centerTitle: true,
-      ),
+      backgroundColor: Colors.grey.shade800,
+        appBar: AppBar(
+          title: Text(widget.name),
+          centerTitle: true,
+        ),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Stack(
+              alignment: Alignment.topCenter,
+              children: [
+                Padding(
+                  padding:  EdgeInsets.only(top: MediaQuery.of(context).size.height * .067),
+                  child: Card(
+                    color: Colors.grey.shade700,
+                    child: Column(
+                      children: [
+                        SizedBox(height: MediaQuery.of(context).size.height * .06,),
+                        ReusableRow(title: 'Cases', value: widget.totalCases.toString(),),
+                        ReusableRow(title: 'Recovered', value:  widget.totalRecovered.toString(),),
+                        ReusableRow(title: 'Death', value:  widget.totalDeaths.toString(),),
+                        ReusableRow(title: 'Critical', value: widget.critical.toString(),),
+                        ReusableRow(title: 'Today Recovered', value:widget.totalRecovered.toString(),),
+
+                      ],
+                    ),
+                  ),
+                ),
+                Positioned(
+                  child: CircleAvatar(
+                    radius: 50,
+                    backgroundImage: NetworkImage(widget.image),
+                  ),
+                ),
+              ],
+          )
+        ],
+        ),
     );
   }
 }
